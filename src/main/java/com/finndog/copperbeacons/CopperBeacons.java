323425d17d47cac9b020f07f2cc251e5d046de80
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2022-07-06 18:18:19 +0100

    updated beacon_base_blocks.json, beacon_payment_items.json and CopperBeacons.java

---
+++ src/main/java/com/finndog/copperbeacons/CopperBeacons.java
@@ -0,0 +1,10 @@
+package com.finndog.copperbeacons;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CopperBeacons implements ModInitializer{
+    @Override
+    public void onInitialize() {
+
+    }
+}
